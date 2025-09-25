6c0ccaf1bd4915139db80258a959e57f1b84a992
Sergio Lucas <155183770+SergioLucas099@users.noreply.github.com>
2025-09-25 11:15:10 -0500

    Avances 25-09-2025

--- app/src/main/java/com/example/manodelartesanogestionturnostv/Model/TurnosModel.kt
+++ app/src/main/java/com/example/manodelartesanogestionturnostv/Model/TurnosModel.kt
@@ -1,6 +1,7 @@
 package com.example.manodelartesanogestionturnostv.Model
 
 class TurnosModel (
+    var id: String? = null,
     var nombre: String? = null,
     var turno: String? = null,
     var tiempo: String? = null,
